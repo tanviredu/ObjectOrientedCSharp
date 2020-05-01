ffd3a6ca986d0b5b1712bd7aed7ef6bbc3e1d4a7
tanviredu <ornobtanvir.git@gmail.com>
2020-05-01 17:43:15 +0600

    initial commit

---
+++ ACM.BL/ACM.BL.TEST/CustomerTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ACM.BL.TEST
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void ValidFullNameTest()
+        {
+
+        }
+    }
+}
