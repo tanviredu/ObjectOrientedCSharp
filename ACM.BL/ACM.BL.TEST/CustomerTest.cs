2e656066f42752bad11793be85672e41c8265e98
tanviredu <ornobtanvir.git@gmail.com>
2020-05-02 05:16:42 +0600

    added second unit test

--- ACM.BL/ACM.BL.TEST/CustomerTest.cs
+++ ACM.BL/ACM.BL.TEST/CustomerTest.cs
@@ -1,6 +1,6 @@
 using System;
 using Xunit;
-
+using ACM.BL;
 namespace ACM.BL.TEST
 {
     public class CustomerTest
@@ -9,6 +9,65 @@ namespace ACM.BL.TEST
         public void ValidFullNameTest()
         {
 
+            Customer customer = new Customer
+            {
+                // setting the properties
+                FirstName = "Tanvir",
+                LastName = "Rahman"
+            };
+            string expectedName = "Rahman Tanvir";
+            string realOutput = customer.FullName;
+
+            //now assert
+            Assert.Equal(expectedName, realOutput);
         }
+        [Fact]
+        public void CustomerInvalidFullNameTest()
+        {
+            Customer customer = new Customer
+            {
+                // setting the properties
+                FirstName = "Tanvir",
+                LastName = "Rahman"
+            };
+            string expectedName = "Tanvir Rahman";
+            string realOutput = customer.FullName;
+
+            //now assert
+            Assert.NotEqual(expectedName, realOutput);
+
+
+
+        }
+
+
+        ///this is where we check the is empty
+        /// if the string is empty how to test it
+        /// // or feed thepart of the value
+        
+        [Fact]
+        public void FullNameFirstNameEmpty()
+        {
+
+            // init with a lst name
+            // it must detect the empty First name
+            Customer customer = new Customer
+            {
+                LastName = "Hosen"
+            };
+
+            string expectedValue = "Hosen";
+            string actuaValue = customer.FullName;
+
+            Assert.Equal(expectedValue, actuaValue);
+
+
+        }
+        [Fact]
+        public void FullNameLastNameEmpty() { 
+        
+        }
+
     }
+
 }
--- ACM.BL/ACM.BL/Customer.cs
+++ ACM.BL/ACM.BL/Customer.cs
@@ -2,11 +2,36 @@
 
 namespace ACM.BL
 {
-    class Program
+    public class Customer
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        
+        // this is Customer type
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CustomerId { get; private set; }
+        public string FullName {
+
+            get
+            {
+                //return FirstName + " " + LastName;
+                // changing the full name property
+                string fullname = LastName;
+                if (!string.IsNullOrEmpty(FirstName)) {
+                        // if the first name is filled
+                    if (!string.IsNullOrEmpty(fullname)) {
+                        // and the second name is filled
+                        // for now full name is the lastname
+                        // added a space with the full name
+                        // then add the First name
+                        fullname += " ";
+                    }
+                    // then add it with the lastname which is full name
+                    fullname += FirstName;
+                }
+
+                return fullname;
+            }
         }
     }
 }
