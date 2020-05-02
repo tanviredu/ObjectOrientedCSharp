1c726e9b8a0b52151c25b5882d500d2107772080
tanviredu <ornobtanvir.git@gmail.com>
2020-05-02 13:34:38 +0600

    added the address class and unit test

---
+++ ACM.BL/ACM.BL.TEST/AddressTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+using ACM.BL;
+
+namespace ACM.BL.TEST
+{
+    public class AddressTest
+    {
+        [Fact]
+        public void PostalCodeValid()
+        {
+            Address address = new Address(1)
+            {
+                PostalCode = "1234"
+            };
+
+            Assert.True(address.Validate());
+
+        }
+
+        [Fact]
+        public void PostalCodeInvalid()
+        {
+            Address address = new Address(1)
+            {
+                PostalCode = null
+            };
+
+            Assert.False(address.Validate());
+        }
+
+    }
+}
---
+++ ACM.BL/ACM.BL/Address.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACM.BL
+{
+   public  class Address
+    {
+        public Address()
+        {
+
+        }
+        public Address(int addressid){
+            AddressId = addressid;
+        }
+
+        // setting all the attribute for the 
+        // address class
+        
+        public int AddressId { get; private set; }
+        public int AddressType { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string State { get; set; }
+        public string StreetLine1 { get; set; }
+        public string StreetLine2 { get; set; }
+
+
+        public bool Validate(){
+
+            var isValid = true;
+            if (PostalCode == null) isValid = false;
+            if (AddressId <= 0) isValid = false;
+            return isValid;
+        }
+
+    }
+}
